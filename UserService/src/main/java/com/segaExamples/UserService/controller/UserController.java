854f6fca94b826d3167c94444e929eae3a4d9e09
Abhijeet-2019 <abhijeet.mohanty001@gmail.com>
2025-03-01 12:03:30 +0530

    Added the order class

--- UserService/src/main/java/com/segaExamples/UserService/controller/UserController.java
+++ UserService/src/main/java/com/segaExamples/UserService/controller/UserController.java
@@ -5,11 +5,18 @@ import com.segaExamples.UserService.models.Users;
 import com.segaExamples.UserService.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3030")
 @RequestMapping("user")
 public class UserController {
 
@@ -30,19 +37,29 @@ public class UserController {
     }
 
 
-    /**
-     * This method is used to validate the user
-     * @param email    :
-     *                  Valid email for the user.
-     * @param password :
-     *                 Valid password for the user.
-     * @return :
-     *              An instance of Users Object.
-     *
-     */
-    @GetMapping("/validateLogin")
-    public Users validateUser(@RequestParam String email, @RequestParam String password) {
-        log.info("The received user name == {} &&  password is  --{}", email, password);
-        return userService.validateUser(email, password);
+    @PostMapping("/validateLogin")
+    public ResponseEntity validateUser(@RequestBody Map<String, String> requestBody) {
+        log.info("Checking the details  _+"+requestBody);
+        String email = requestBody.get("email");
+        String password = requestBody.get("password");
+        Users userDetails  = userService.validateUser(email, password);
+        if(userDetails == null){
+            return (ResponseEntity) ResponseEntity.notFound();
+        }
+        return ResponseEntity.ok().body(userDetails);
     }
+
+    /**
+     * This method is used to fetch all user in DB.
+     * @return
+     */
+
+    @RequestMapping("/fetchAllData")
+    public ResponseEntity fetchAllUsers(){
+       List<Users> fetchAllUsers= userService.fetchAllUsers();
+       if(fetchAllUsers.isEmpty()){
+           return ResponseEntity.ok().body("No Data found");
+       }
+       return ResponseEntity.ok().body(fetchAllUsers);
+   }
 }
